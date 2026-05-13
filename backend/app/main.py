96952bdd597c4cceba5a57146748e11351fe7036
chadowsv <lisethsbv2013@gmail.com>
2026-05-13 04:56:04 -0500

    duh

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app=FastAPI(title="Youtube",lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]    
